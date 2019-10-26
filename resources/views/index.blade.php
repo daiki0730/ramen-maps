d233da0a663f18bb793055c2f08a92e39115d7dd
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-26 20:53:21 +0900

    viewファイルを作成
    仮のコードを記入

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Ramen Maps</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Ramen Maps</h1>
+    <ul>
+      <li><a href="">家系</a></li>
+      <li><a href="">二郎系</a></li>
+      <li><a href="">醤油系</a></li>
+    </ul>
+  </div>
+</body>
+</html>
