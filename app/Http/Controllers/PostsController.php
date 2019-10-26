cee61c287d764c524fc2de74155ada13941b59dd
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-26 21:31:17 +0900

    implicit bindingを利用
    idの引用が必要なくなったため記述を変更

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -11,4 +11,9 @@ class PostsController extends Controller
       $posts = Post::latest()->get();
       return view('posts.index')->with('posts', $posts);
     }
+
+    public function show(Post $post) {
+      $post = Post::findOrFail($id);
+      return view('posts.show')->with('post', $post);
+    }
 }
