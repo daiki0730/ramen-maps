329b8544ccd3c275bfe90c90f32e412db926dc9c
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-26 21:11:49 +0900

    データを取得する記述を追加
    ->withを使いview内でpostsを利用できるようにする

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function index() {
-      return view('posts.index');
+      $posts = Post::latest()->get();
+      return view('posts.index')->with('posts', $posts);
     }
 }
