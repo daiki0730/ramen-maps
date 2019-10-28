2f96c6a31c6c8b96cf338297ed4a64deecd54dee
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-28 18:37:18 +0900

    名前空間useを使用
    クラスや関数などの名前の衝突を防ぐため

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use App\Http\Requests\PostRequest;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
